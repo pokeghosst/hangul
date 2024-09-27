f201a806fc1ac189a40b98bc20b7555e55f4d06e
pokeghost <pokeghost@disroot.org>
2024-09-27 16:15:50 +0300

    Add alphabet

---
+++ src/components/AlphabetTable.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import alphabet from '$lib/alphabet';
+</script>
+
+<table>
+	<tbody>
+		{#each alphabet as category}
+			<tr>
+				<td>
+					<input type="checkbox" bind:checked={category.selected} />
+				</td>
+				{#each category.letters as letter}
+					<td><span>{letter.letter}</span><br /><span>{letter.romanization}</span></td>
+				{/each}
+			</tr>
+		{/each}
+	</tbody>
+</table>
+
+<style>
+	table {
+		border-collapse: collapse;
+		border: 1px solid #ddd;
+		table-layout: fixed;
+		width: 90%;
+		max-width: 800px;
+		margin: auto;
+	}
+
+	td {
+		border: 1px solid #ddd;
+		padding: 6px;
+		text-align: center;
+	}
+</style>
---
+++ src/lib/alphabet.ts
@@ -0,0 +1,64 @@
+const alphabet = [
+	{
+		selected: true,
+		letters: [
+			{ letter: 'ㅏ', romanization: ['a'] },
+			{ letter: 'ㅓ', romanization: ['eo'] },
+			{ letter: 'ㅗ', romanization: ['o'] },
+			{ letter: 'ㅜ', romanization: ['u'] },
+			{ letter: 'ㅡ', romanization: ['eu'] },
+			{ letter: 'ㅣ', romanization: ['i'] },
+			{ letter: 'ㅐ', romanization: ['ae'] },
+			{ letter: 'ㅔ', romanization: ['e'] },
+			{ letter: 'ㅚ', romanization: ['oe'] },
+			{ letter: 'ㅟ', romanization: ['wi'] }
+		]
+	},
+	{
+		selected: false,
+		letters: [
+			{ letter: 'ㅑ', romanization: ['ya'] },
+			{ letter: 'ㅕ', romanization: ['yeo'] },
+			{ letter: 'ㅛ', romanization: ['yo'] },
+			{ letter: 'ㅠ', romanization: ['yu'] },
+			{ letter: 'ㅒ', romanization: ['yae'] },
+			{ letter: 'ㅖ', romanization: ['ye'] },
+			{ letter: 'ㅘ', romanization: ['wa'] },
+			{ letter: 'ㅙ', romanization: ['wae'] },
+			{ letter: 'ㅝ', romanization: ['wo'] },
+			{ letter: 'ㅞ', romanization: ['we'] },
+			{ letter: 'ㅢ', romanization: ['ui'] }
+		]
+	},
+	{
+		selected: false,
+		letters: [
+			{ letter: 'ㄱ', romanization: ['g', 'k'] },
+			{ letter: 'ㄲ', romanization: ['kk'] },
+			{ letter: 'ㅋ', romanization: ['k'] },
+			{ letter: 'ㄷ', romanization: ['d', 't'] },
+			{ letter: 'ㄸ', romanization: ['tt'] },
+			{ letter: 'ㅌ', romanization: ['t'] },
+			{ letter: 'ㅂ', romanization: ['b', 'p'] },
+			{ letter: 'ㅃ', romanization: ['pp'] },
+			{ letter: 'ㅍ', romanization: ['p'] }
+		]
+	},
+	{
+		selected: false,
+		letters: [
+			{ letter: 'ㅈ', romanization: ['j'] },
+			{ letter: 'ㅉ', romanization: ['jj'] },
+			{ letter: 'ㅊ', romanization: ['ch'] },
+			{ letter: 'ㅅ', romanization: ['s'] },
+			{ letter: 'ㅆ', romanization: ['ss'] },
+			{ letter: 'ㅎ', romanization: ['h'] },
+			{ letter: 'ㄴ', romanization: ['n'] },
+			{ letter: 'ㅁ', romanization: ['m'] },
+			{ letter: 'ㅇ', romanization: ['ng'] },
+			{ letter: 'ㄹ', romanization: ['r', 'l'] }
+		]
+	}
+];
+
+export default alphabet;
