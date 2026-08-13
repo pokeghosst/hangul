09cbfb680ebdbd052850a366f1770bf635f480cd
pokeghost <pokeghost@disroot.org>
2026-08-13 16:16:05 +0300

    feat: add new header and some Capriccio styling

---
+++ src/components/Header.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { page } from '$app/state';
+
+	const navigation = [
+		{ href: '/', label: 'Practice' },
+		{ href: '/table', label: 'Alphabet' },
+		{ href: '/settings', label: 'Settings' }
+	];
+</script>
+
+<header>
+	<div>
+		<h1>Learn Hangul</h1>
+
+		<nav>
+			{#each navigation as navLink}
+				<a href={navLink.href} class:active={page.url.pathname === navLink.href}>{navLink.label}</a>
+			{/each}
+		</nav>
+	</div>
+</header>
